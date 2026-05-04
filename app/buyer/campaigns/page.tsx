42d279a03f962bc82df081880a79379338970d72
youssefalw2001 <youssefalw2001@gmail.com>
2026-05-04 23:43:30 +0300

    Force dynamic rendering for buyer campaigns page

--- app/buyer/campaigns/page.tsx
+++ app/buyer/campaigns/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { DashboardShell } from "@/components/DashboardShell";
 import { getCampaignCards } from "@/lib/queries";
 
+export const dynamic = "force-dynamic";
+
 export default async function BuyerCampaignsPage() {
   const campaigns = await getCampaignCards();
 
