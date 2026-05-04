1217172d28c4debe4151a64608cf424a7f466b82
youssefalw2001 <youssefalw2001@gmail.com>
2026-05-04 23:44:50 +0300

    Force dynamic rendering for admin clips page

--- app/admin/clips/page.tsx
+++ app/admin/clips/page.tsx
@@ -3,6 +3,8 @@ import { approveClipAction, rejectClipAction } from "@/app/actions";
 import { DashboardShell } from "@/components/DashboardShell";
 import { getClipRows } from "@/lib/queries";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminClipsPage() {
   const clips = await getClipRows();
 
