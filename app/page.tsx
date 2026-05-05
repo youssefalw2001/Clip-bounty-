539a3a4b260daa82bbb86f470590e4d96fccd2b8
youssefalw2001 <youssefalw2001@gmail.com>
2026-05-05 23:03:53 +0300

    Redirect homepage to GhostWallet

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import GhostWalletPage from "@/app/ghost/page";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return <GhostWalletPage />;
+  redirect("/ghost");
 }
